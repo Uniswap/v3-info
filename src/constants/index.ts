a5d83fbb4bf57e4b8009f223fe80d77b448f7675
ianlapham <ianlapham@gmail.com>
2022-03-08 13:23:32 -0500

    hotfix

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -36,11 +36,16 @@ export const NETWORK_ONLY = {
 }
 
 // temporary! fixing USD accounting on subgraph - open issue if urgent
-export const TOKEN_HIDE = ['0xd46ba6d942050d489dbd938a2c909a5d5039a161', '0x7dfb72a2aad08c937706f21421b15bfc34cba9ca']
+export const TOKEN_HIDE = [
+  '0xd46ba6d942050d489dbd938a2c909a5d5039a161',
+  '0x7dfb72a2aad08c937706f21421b15bfc34cba9ca',
+  '0x12b32f10a499bf40db334efe04226cca00bf2d9b',
+]
 export const POOL_HIDE = [
   '0x86d257cdb7bc9c0df10e84c8709697f92770b335',
   '0xf8dbd52488978a79dfe6ffbd81a01fc5948bf9ee',
   '0x8fe8d9bb8eeba3ed688069c3d6b556c9ca258248',
+  '0xa850478adaace4c08fc61de44d8cf3b64f359bec',
 ]
 
 export interface WalletInfo {

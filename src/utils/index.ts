87eb4d787030c42609543f4a355572c54ec81965
PEPO <47273814+vPEPO@users.noreply.github.com>
2023-03-14 16:36:28 -0300

    Update CeloNetworkInfo (#260)
    Switch the explorer from https://explorer.celo.org/ to https://celoscan.io/

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -39,7 +39,7 @@ export function getEtherscanLink(
     networkVersion === PolygonNetworkInfo
       ? 'https://polygonscan.com/'
       : networkVersion === CeloNetworkInfo
-      ? 'https://explorer.celo.org'
+      ? 'https://celoscan.io/'
       : networkVersion === ArbitrumNetworkInfo
       ? 'https://arbiscan.io/'
       : networkVersion === OptimismNetworkInfo

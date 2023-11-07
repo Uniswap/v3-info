3d08b81d62a72c16976c216c63e8e5eea9ac03a2
eddie <66155195+just-toby@users.noreply.github.com>
2023-11-07 12:52:56 -0800

    fix: remove userAgent

--- src/utils/userAgent.ts
+++
@@ -1,7 +0,0 @@
-import { UAParser } from 'ua-parser-js'
-
-const parser = new UAParser(window.navigator.userAgent)
-const { type } = parser.getDevice()
-const platform = parser.getOS().name
-const isIOS = platform === 'iOS'
-export const isNonIOSPhone = !isIOS && type === 'mobile'
